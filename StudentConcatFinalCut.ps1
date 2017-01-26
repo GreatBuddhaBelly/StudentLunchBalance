82597ac55715cbb660062add51f6a0c1ed24d836
GreatBuddhaBelly <peter.bitante@gmail.com>
2017-01-26 14:15:07 -0600

    Finished
    Final product, could be improved

--- StudentConcatFinalCut.ps1
+++ StudentConcatFinalCut.ps1
@@ -12,10 +12,10 @@
 
 
 <#
-  Parameters:
-  $inFile : imports the .csv of the specified path and sorts them according to address in descending order
-  $addressHash, $noDataHash, $finalStudentBalanceHash : 3 hashtables to hold the data according to their names
-  $hashCount, $noDataCount : counters that are kept for debugging purposes
+Parameters:
+$inFile : imports the .csv of the specified path and sorts them according to address in descending order
+$addressHash, $noDataHash, $finalStudentBalanceHash : 3 hashtables to hold the data according to their names
+$hashCount, $noDataCount : counters that are kept for debugging purposes
 #>
 ##################################################
 $addressHash = @{}
@@ -24,112 +24,108 @@ $finalStudentBalanceHash = @{}
 $studentsArray = @()
 $hashCount =0
 $noDataCount =0
+$outputFilePath = Read-Host -Prompt "Please enter the output file path"
 ##################################################
-
-
 Clear-Host
+
+
+
 <#
-  Function: fileImport
-  Takes the .csv specified at head of script and parses out addresses that have adequate data and adds them to a hashtable.
-  This function also parses out data with a lack of address and adds them to a seperate hashtable
+Function: fileImport
+Takes the .csv specified at head of script and parses out addresses that have adequate data and adds them to a hashtable.
+This function also parses out data with a lack of address and adds them to a seperate hashtable
 #>
 function fileImport{
-
 Param(
-  [String]$filePath = $args
+  [String]$inFilePath = $args
 )
 
-    if ($filePath.Equals("")){
-      $filePath = Read-Host -Prompt "Please enter the file path including the file"
+    # Checks file path
+    if ($inFilePath.Equals("")){
+        $inFilePath = Read-Host -Prompt "Please enter the file path including the file"
 
-        if ($filePath.Equals("")){
-          Write-Host "  You did not enter a file path. Please run the script again."
+            if ($inFilePath.Equals("")){
+            Write-Host "  You did not enter a file path. Please run the script again."
         }
     }
-#    $inFile = Import-Csv "D:\StudentLunchBalance\massagedStudentBalance.csv" -Header StudentID, FirstName, LastName, Address, PhoneNumber, Balance | sort Address -Descending
-    $inFile = Import-Csv $filePath -Header StudentID, FirstName, LastName, Address, PhoneNumber, Balance | sort Address -Descending
+
+
+    $inFile = Import-Csv $inFilePath -Header StudentID, FirstName, LastName, Address, PhoneNumber, Balance | sort Address -Descending
+
     foreach ($studentLine in $inFile){
 
+        if ($studentLine.Balance -ne ""){
+            # converts balance from pennies to dollars
+            $studentLine.Balance = $studentLine.Balance/100
 
-      if ($studentLine.Balance -ne ""){
-        # converts balance from pennies to dollars
-        $studentLine.Balance = $studentLine.Balance/100
-
-  #  sorts out null values of addresses
-  if ($studentLine.Address -ne ""){
-
-      $addressHash[$studentLine.Address] += @($studentLine)
-      $hashCount++
-  }
-}
-else{
+            #  sorts out null values of addresses
+            if ($studentLine.Address -ne ""){
+                $addressHash[$studentLine.Address] += @($studentLine)
+                $hashCount++
+            }
+        }
+        else{
             $noDataHash[$studentLine.StudentID] += @($studentLine)
             $noDataCount++
-
         }
-
     }
-  return $addressHash
 }
 
 <#
-  Function: createKeyArray
-  Creates an array of all addresses with sufficient data, returns the array to be used as keys
+Function: createKeyArray
+Creates an array of all addresses with sufficient data, returns the array to be used as keys
 #>
 function createKeyArray{
     $keysArray = @()
 
     foreach ($address in $addressHash.Keys){
-            $keysArray += $address
-          }
+        $keysArray += $address
+    }
     return $keysArray
 }
 
-<#
-  Function: calculateBalance
-  Calculates the final balance for the given address, returns the final balance
-#>
-function calculateBalance{
-    param( [String]$addressIn )
-
-    $hashKey = $addressHash.Get_Item($addressIn)
-
-    $finalBalance = 0
-
-    foreach ($studentObject in $hashKey){
-
-        $finalBalance += $studentObject.Balance
-    }
-
-    return $finalBalance
-}
 
 <#
-  Function: hashSearch
-  Uses createKeyArray to create an array of all addresses that have sufficient data. Then
-  iterates through the array and uses the calculateBalance function with given the parameter
-  of the address. This then adds the given key and the calculated final balance to a hashtable, which is written to a .csv
+Function: hashSearch
+Uses createKeyArray to create an array of all addresses that have sufficient data. Then
+iterates through the array and uses the calculateBalance function with given the parameter
+of the address. This then adds the given key and the calculated final balance to a hashtable, which is written to a .csv
 #>
 
 function hashSearch{
 
     $keys = createKeyArray
-    foreach ($key in $keys){
 
-    $addressTotalBalance = @()
+    foreach ($key in $keys){
+        $addressTotalBalance = @()
+        $studentPerKeyCount = 0
+        $finalBalancePerAddress = 0
 
         foreach ($student in $addressHash.Get_Item($key)){
-
-        $studentAddress = $($student).Address
-            
-            Write-Host "  Student object:" $student.FirstName " " $student.LastName
-            Write-Host "  Student Address:" $studentAddress `n
-            
+            $studentObj = New-Object PSObject
+            $studentPerKeyCount++
+            $studentAddress = $($student).Address
+            $finalBalancePerAddress += $($student).Balance
+            <#  Used for debugging student information to ensure proper data output
+            #          Write-Host "  Student object:" $student.FirstName " " $student.LastName
+            #          Write-Host "  Student Address:" $studentAddress `n
+            #>
         }
-    
+
+        # declaring the headers and data for each line in the output .csv
+        $studentObj | add-member -membertype NoteProperty -name Address -value $key
+
+        $studentObj | add-member -membertype NoteProperty -name "Number of Students" -value $studentPerKeyCount
+
+        $studentObj | add-member -membertype NoteProperty -name "Final Balance" -value $finalBalancePerAddress
+
+        $studentObj | Export-CSV $outputFilePath -Append -NoTypeInformation
+
+        $studentObj = $null
+        #        Write-Host "  Students per address with balance at: "$key "  Number of kids: "$studentPerKeyCount "  Total Balance: "$finalBalancePerAddress
+
     }
-    return $finalStudentBalanceHash
-   }
+}
 
 
 ##################################################### FileImport works as a function, must get individual student objects out of hashtable to
